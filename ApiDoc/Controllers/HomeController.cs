3ea160bad47efccbe210e070ade4baa917450aa9
david <davidguo.developer@outlook.com>
2015-06-05 14:28:00 -0700

    init

---
+++ ApiDoc/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ApiDoc.Models;
+
+namespace ApiDoc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            List<IApiDoc> apiDocs = new List<IApiDoc>();
+            apiDocs = ApiDocReporter.LoadHtmlApiDocFromFolder(Server.MapPath("~/HTML_Report"));
+            return View(apiDocs);
+        }
+
+    }
+}
